de4eb697e913eba0a109fdf588d434f8c2c5190c
Pete1281 <30956172+Pete1281@users.noreply.github.com>
2017-12-25 09:07:17 -0600

    fixes a configuration issue when rolling back from 5.1 to 5.0.0.1

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,11 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+# config.load_defaults 5.1
 
 module LearnCalendarRailsReact
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
